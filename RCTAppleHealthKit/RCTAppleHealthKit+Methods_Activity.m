3d0575725276c3e81be288a98b8319384450a257
Jimmy Cook <wjimmycook@gmail.com>
2020-05-30 09:04:59 -0400

    Update RCTAppleHealthKit+Methods_Activity.m

--- RCTAppleHealthKit/RCTAppleHealthKit+Methods_Activity.m
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_Activity.m
@@ -125,36 +125,37 @@
     NSMutableArray *workoutSamples = [NSMutableArray new];
     HKQuantity *workoutDistance = nil;
     
-//     // create energy burn sample
-//     HKQuantity *kcalQuantity = [HKQuantity quantityWithUnit:[HKUnit unitFromString:@"kcal"] doubleValue:calories];
-//     HKQuantityType *kcalType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierActiveEnergyBurned];
-//     HKQuantitySample *kcalSample = [HKQuantitySample quantitySampleWithType:kcalType
-//                                                                    quantity:kcalQuantity
-//                                                                   startDate:startDate
-//                                                                     endDate:endDate
-//                                                                    metadata:@{@"reportId": reportId}
-//                                     ];
+    // create energy burn sample
+    HKQuantity *kcalQuantity = [HKQuantity quantityWithUnit:[HKUnit unitFromString:@"kcal"] doubleValue:calories];
+    HKQuantityType *kcalType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierActiveEnergyBurned];
+    HKQuantitySample *kcalSample = [HKQuantitySample quantitySampleWithType:kcalType
+                                                                   quantity:kcalQuantity
+                                                                  startDate:startDate
+                                                                    endDate:endDate
+                                                                   metadata:@{@"reportId": reportId}
+                                    ];
     
-//     [workoutSamples addObject:kcalSample];
+    [workoutSamples addObject:kcalSample];
     
-    // create distance covered sample
-//     if (distance != nil) {
-//         HKQuantity *distanceQuantity = [HKQuantity quantityWithUnit:[HKUnit unitFromString:@"m"] doubleValue:[distance doubleValue]];
-//         HKQuantitySample *distanceSample = [HKQuantitySample quantitySampleWithType:[self distanceTypeForType:activityType]
-//                                                                            quantity:distanceQuantity
-//                                                                           startDate:startDate
-//                                                                             endDate:endDate
-//                                                                            metadata:@{@"reportId": reportId}
-//                                             ];
+    create distance covered sample
+    if (distance != nil) {
+        HKQuantity *distanceQuantity = [HKQuantity quantityWithUnit:[HKUnit unitFromString:@"m"] doubleValue:[distance doubleValue]];
+        HKQuantitySample *distanceSample = [HKQuantitySample quantitySampleWithType:[self distanceTypeForType:activityType]
+                                                                           quantity:distanceQuantity
+                                                                          startDate:startDate
+                                                                            endDate:endDate
+                                                                           metadata:@{@"reportId": reportId}
+                                            ];
         
-//         [workoutSamples addObject:distanceSample];
-//     }
+        [workoutSamples addObject:distanceSample];
+    }
     
     // create the workout
     HKWorkout *workout = [HKWorkout workoutWithActivityType:activityType
                                                   startDate:startDate
                                                     endDate:endDate
-                                          totalEnergyBurned:nil
+                                              workoutEvents:nil
+                                          totalEnergyBurned:kcalQuantity
                                               totalDistance:nil
                                                    metadata:@{@"Template": template}
                           ];
